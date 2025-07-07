c8583aad9a94948adb0c58a90d83fa8963869802
Harsh <harshwardhandatascientist@gmail.com>
2025-07-07 18:33:04 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "terraform-drift-demo-bucket"
+ bucket = "terraform-drift-demo-bucket-${random_id.suffix.hex}"
   acl    = "private"
 }
